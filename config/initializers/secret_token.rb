dad0b14bce3173dec4868700b7c0ceae123699e5
René Gábora <gabre71@gmail.com>
2013-05-31 13:50:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mychamps::Application.config.secret_token = '7371b418997106dbf80640bcae7e0c12ebacebd718403ba9cd5f6206edc0b1f5dd4a223d0c0800df3ae001b331c52e5e6c1752bdb23dabf0d84326f12c9b3d77'
